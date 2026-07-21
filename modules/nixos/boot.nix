11320648030d63a47dd2a05e5399b34ae6a0f983
nka <novalkatlas@gmail.com>
2026-07-21 16:37:24 +0800

    Added Module: modules/nixos/boot.nix

---
+++ modules/nixos/boot.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+
+let
+cfg = config.monixes.system.boot;
+in {
+    options.monixes.system.boot = {
+        enable = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Enable Limine bootloader configuration.";
+        };
+    };
+
+    config = lib.mkIf cfg.enable {
+        boot.loader.limine.enable = true;
+        boot.loader.efi.canTouchEfiVariables = true;
+    };
+}
