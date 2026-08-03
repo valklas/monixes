86b3f5227e45b435225e5ea691c02a23fefb2e40
nka <novalkatlas@gmail.com>
2026-08-03 13:44:19 +0800

    Refactored: modules/nixos/desktop/DM/greetd-tuigreet.nix to single-choice enum pattern

--- modules/nixos/desktop/DM/greetd-tuigreet.nix
+++ modules/nixos/desktop/DM/greetd-tuigreet.nix
@@ -1,23 +1,9 @@
 { config, lib, pkgs, ... }:
 
 let
-cfg = config.monixes.system.desktop.DM.greetd-tuigreet;
+cfg = config.monixes.system.desktop.DM;
 in {
-    options.monixes.system.desktop.DM.greetd-tuigreet = {
-        enable = lib.mkOption {
-            type = lib.types.bool;
-            default = false;
-            description = "Enable greetd display manager with tuigreet.";
-        };
-
-        environment = lib.mkOption {
-            type = lib.types.str;
-            default = "bash";
-            description = "The target user environment name or custom startup command (e.g. start-hyprland).";
-        };
-    };
-
-    config = lib.mkIf cfg.enable (
+    config = lib.mkIf (cfg.displayManager == "greetd-tuigreet") (
         let
             cmd = if cfg.environment == "plasma-wayland" then
                     "startplasma-wayland"
