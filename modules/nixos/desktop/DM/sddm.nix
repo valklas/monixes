5d47a0601fdca99ae3efeb23e089196a4435c40b
nka <novalkatlas@gmail.com>
2026-08-03 13:45:21 +0800

    Refactored: modules/nixos/desktop/DM/sddm.nix to single-choice enum pattern

--- modules/nixos/desktop/DM/sddm.nix
+++ modules/nixos/desktop/DM/sddm.nix
@@ -1,17 +1,9 @@
 { config, lib, ... }:
 
 let
-cfg = config.monixes.system.desktop.DM.sddm;
+cfg = config.monixes.system.desktop.DM;
 in {
-    options.monixes.system.desktop.DM.sddm = {
-        enable = lib.mkOption {
-            type = lib.types.bool;
-            default = false;
-            description = "Enable SDDM display manager.";
-        };
-    };
-
-    config = lib.mkIf cfg.enable {
+    config = lib.mkIf (cfg.displayManager == "sddm") {
         services.displayManager.sddm = {
             enable = lib.mkDefault true;
             wayland.enable = lib.mkDefault true;
