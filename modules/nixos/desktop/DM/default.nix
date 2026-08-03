6b07eda36a62f8515d016ffa1e7d746f73ad37e5
nka <novalkatlas@gmail.com>
2026-08-03 13:44:07 +0800

    Refactored: modules/nixos/desktop/DM/default.nix to single-choice enum pattern

--- modules/nixos/desktop/DM/default.nix
+++ modules/nixos/desktop/DM/default.nix
@@ -1,8 +1,22 @@
-{ ... }:
+{ config, lib, ... }:
 
 {
     imports = [
         ./greetd-tuigreet.nix
         ./sddm.nix
     ];
+
+    options.monixes.system.desktop.DM = {
+        displayManager = lib.mkOption {
+            type = lib.types.enum [ "none" "greetd-tuigreet" "sddm" ];
+            default = "none";
+            description = "The display manager to enable.";
+        };
+
+        environment = lib.mkOption {
+            type = lib.types.str;
+            default = "bash";
+            description = "The target user environment or custom startup command for greetd-tuigreet (e.g. start-hyprland).";
+        };
+    };
 }
