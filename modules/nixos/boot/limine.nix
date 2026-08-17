3409e7feb7d007d34c1ad92f66559a833a1e1ee0
nka <novalkatlas@gmail.com>
2026-08-17 20:49:27 +0800

    Add Limine enable option

--- modules/nixos/boot/limine.nix
+++ modules/nixos/boot/limine.nix
@@ -3,15 +3,21 @@
 let
     cfg = config.monixes.system.boot;
 in {
+    options.monixes.system.boot.limine.enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable the Limine bootloader.";
+    };
+
     options.monixes.system.boot.canTouchEfiVariables = lib.mkOption {
         type = lib.types.bool;
         default = true;
         description = "Allow modification of EFI variables.";
     };
 
-    config = {
+    config = lib.mkIf cfg.limine.enable {
         boot.loader.limine = {
-            enable = true;
+            enable = cfg.limine.enable;
             style.interface.branding = lib.mkDefault "Monixes Bootloader";
         };
         boot.loader.efi.canTouchEfiVariables = cfg.canTouchEfiVariables;
