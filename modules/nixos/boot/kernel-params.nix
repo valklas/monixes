32d9d7f531500877fbe0009cf3bdef10e5b7e061
nka <novalkatlas@gmail.com>
2026-08-18 14:49:07 +0800

    Preserve default kernel parameters

--- modules/nixos/boot/kernel-params.nix
+++ modules/nixos/boot/kernel-params.nix
@@ -1,12 +1,18 @@
 { config, lib, ... }:
 
-{
+let
+    cfg = config.monixes.system.boot;
+in {
     options.monixes.system.boot.kernelParams = lib.mkOption {
         type = lib.types.listOf lib.types.str;
-        default = [ "quiet" "splash" ];
+        default = [ ];
         description = "Additional kernel parameters to pass at boot.";
-        example = [ "quiet" "splash" ];
+        example = [ "kvm-intel" "tun" ];
     };
 
-    config.boot.kernelParams = lib.mkAfter config.monixes.system.boot.kernelParams;
+    config.boot.kernelParams = lib.mkAfter (lib.unique (
+        [ "quiet" ]
+        ++ lib.optional cfg.plymouth.enable "splash"
+        ++ cfg.kernelParams
+    ));
 }
